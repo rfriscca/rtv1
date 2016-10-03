33578f2f0d0cb018c1f011bfd7edfde3ffe6e362
Remi FRISCCA <rfriscca@e3r8p6.42.fr>
2016-10-03 15:54:00 +0200

    color adjustment

--- srcs/color.c
+++ srcs/color.c
@@ -6,7 +6,7 @@
 /*   By: rfriscca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/27 16:13:22 by rfriscca          #+#    #+#             */
-/*   Updated: 2016/10/03 13:00:12 by rfriscca         ###   ########.fr       */
+/*   Updated: 2016/10/03 15:53:21 by rfriscca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ t_color		calc_shadow(t_color cobj, t_color clight)
 {
 	t_color		color;
 
-	color.r = (cobj.r * clight.r) * 0.1;
-	color.g = (cobj.g * clight.g) * 0.1;
-	color.b = (cobj.b * clight.b) * 0.1;
+	color.r = (cobj.r * clight.r) * 0.2;
+	color.g = (cobj.g * clight.g) * 0.2;
+	color.b = (cobj.b * clight.b) * 0.2;
 	return (color);
 }
 
@@ -26,21 +26,13 @@ t_color		calc_color(t_color cobj, t_color clight, double angle)
 {
 	t_color		color;
 
-	if (angle > 0.1)
+	if (angle > 0.2)
 	{
 		color.r = (cobj.r * clight.r * angle);
 		color.g = (cobj.g * clight.g * angle);
 		color.b = (cobj.b * clight.b * angle);
 	}
-	else if (angle > 0 && angle < 0.1)
-	{
-		color.r = cobj.r * clight.r * 0.1;
-		color.g = (cobj.g * clight.g) * 0.1;
-		color.b = (cobj.b * clight.b) * 0.1;
-	}
 	else
-	{
 		color = calc_shadow(cobj, clight);
-	}
 	return (color);
 }
